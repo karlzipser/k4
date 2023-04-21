7e56359cb8b10231e2d2619f4234fd1a5e07237f
Karl Zipser <karlzipser@berkeley.edu>
2023-04-21 10:31:26 -0700

    gacp

--- drafts/d0.py
+++ drafts/d0.py
@@ -2,9 +2,8 @@
 from k4.utils.arg import *
 
 dict_in = {
-	'x':int,#5,
-	'y':2.,
-	'hi':str,#'hello'
+	'n':int,
+	's':str,
 }
 
 print('dict_in:',dict_in)
@@ -13,4 +12,4 @@ dict_out = parse_args_to_dict(dict_in)
 
 print('dict_out',dict_out)
 
-print(dict_out['hi'],'Product:', dict_out['x'] * dict_out['y'])
+print(dict_out['n'] * dict_out['s'])
